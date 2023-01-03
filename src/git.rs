d676b9734e46fffe5fdc5870034ff835e43660b2
Abby Henríquez Tejera <paradoja@gmail.com>
2023-01-03 22:49:18 +0100

    Allow to specify a different path for the repository

--- src/git.rs
+++ src/git.rs
@@ -1,9 +1,11 @@
 // This is a git adapter for the git2 crate. it is used to create a git branch
 // in the current repository
 
+use std::path::PathBuf;
+
 use git2::Repository;
 
-fn git_repository_open_from_workdir(path: &str) -> Repository {
+fn git_repository_open_from_workdir(path: PathBuf) -> Repository {
     match Repository::open(path) {
         Ok(repo) => repo,
         Err(e) => panic!("failed to open: {}", e),
@@ -11,8 +13,8 @@ fn git_repository_open_from_workdir(path: &str) -> Repository {
 }
 
 // Create a git branch in the current repository
-pub fn branch_create(repo_path: Option<&str>, branch_name: std::string::String) {
-    let path = repo_path.unwrap_or(".");
+pub fn branch_create(repo_path: Option<PathBuf>, branch_name: std::string::String) {
+    let path = repo_path.unwrap_or(PathBuf::from("."));
     let repo = git_repository_open_from_workdir(path);
     let head = repo.head().unwrap();
     let head_commit = head.peel_to_commit().unwrap();
